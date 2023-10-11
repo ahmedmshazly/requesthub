e7c280286b7c321d5b11db9009e30451cf0b7652
Ahmed <a.mohamed@alustudent.com>
2023-10-11 21:36:18 +0200

    runFile

---
+++ run_project.bat
@@ -0,0 +1,13 @@
+@echo off
+
+rem Navigate to the directory where your main.py is located
+cd /d C:\path\to\your\project
+
+rem Activate your virtual environment (if applicable)
+rem C:\path\to\your\venv\Scripts\activate
+
+rem Run the main.py file
+python ./src/main.py
+
+rem Keep the terminal open after execution (optional)
+cmd /k
